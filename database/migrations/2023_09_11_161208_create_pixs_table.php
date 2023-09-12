7c0fcb45e547c83cfd905b30b64a49b63b0b5051
SandroCast <castrosandro2@gmail.com>
2023-09-11 21:44:00 -0300

    .

--- database/migrations/2023_09_11_161208_create_pixs_table.php
+++ database/migrations/2023_09_11_161208_create_pixs_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('pixs', function (Blueprint $table) {
             $table->id();
             $table->integer('pedido');
-            $table->string('pix');
             $table->timestamps();
         });
     }
